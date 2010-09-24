17aa0341094758c7bd884b08514ba454a1551b69
vitorzachi@gmail.com <vitorzachi@gmail.com@6a19b762-538a-c6d0-a5d2-96b9910417f3>
2010-09-24 23:14:30 +0000

    criadas as classes de TransacaoDebito,TransacaoCredito e Boleto. alterada calsse Transacao para absract.

---
+++ trunk/webservice/src/java/br/unoesc/ws/model/Boleto.java
@@ -0,0 +1,49 @@
+
+package br.unoesc.ws.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author vitor
+ */
+@Entity
+public class Boleto extends GenericModel{
+    @Id
+    private Long id;
+    @OneToOne
+    private TransacaoCredito transacaoCredito;
+    private boolean pago;
+
+    public Boleto() {
+    }
+
+    public boolean isPago() {
+        return pago;
+    }
+
+    public void setPago(boolean pago) {
+        this.pago = pago;
+    }
+
+    public TransacaoCredito getTransacaoCredito() {
+        return transacaoCredito;
+    }
+
+    public void setTransacaoCredito(TransacaoCredito transacaoCredito) {
+        this.transacaoCredito = transacaoCredito;
+    }
+
+    @Override
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+}
---
+++ trunk/webservice/src/java/br/unoesc/ws/model/TransacaoCredito.java
@@ -0,0 +1,14 @@
+
+package br.unoesc.ws.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author vitor
+ */
+@Entity
+public class TransacaoCredito extends Transacao{
+
+    
+}
