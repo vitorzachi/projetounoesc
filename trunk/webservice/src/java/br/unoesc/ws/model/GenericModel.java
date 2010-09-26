f10fbd5cd0dafd9642ba506c404a363edbb486a6
vitorzachi@gmail.com <vitorzachi@gmail.com@6a19b762-538a-c6d0-a5d2-96b9910417f3>
2010-09-26 00:05:45 +0000

    classe de webservice refeita. projeto compilando

--- trunk/webservice/src/java/br/unoesc/ws/model/GenericModel.java
+++ trunk/webservice/src/java/br/unoesc/ws/model/GenericModel.java
@@ -1,14 +1,13 @@
 
 package br.unoesc.ws.model;
 
-import br.unoesc.ws.webService.Property;
 import java.io.Serializable;
 
 /**
  *
  * @author vitor
  */
-public abstract class GenericModel extends Property implements Serializable{
+public abstract class GenericModel implements Serializable{
 
     public GenericModel() {
     }
