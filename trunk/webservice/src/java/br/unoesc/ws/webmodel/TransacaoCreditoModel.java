05278e64a002f822bb6ba25ffa6cd660fa7dfe91
vitorzachi@gmail.com <vitorzachi@gmail.com@6a19b762-538a-c6d0-a5d2-96b9910417f3>
2010-09-28 00:45:08 +0000

    modificado a classe transacaoWS. criado objeto para parametro de transacao de credito para o WS

---
+++ trunk/webservice/src/java/br/unoesc/ws/webmodel/TransacaoCreditoModel.java
@@ -0,0 +1,59 @@
+package br.unoesc.ws.webmodel;
+
+/**
+ *
+ * @author vitor
+ */
+public class TransacaoCreditoModel {
+
+    private Long codCereal;
+    private Long codEmpresa;
+    private String cpfProdutor;
+    private Long numNotaFiscal;
+    private Float qtdComprada;
+
+    public TransacaoCreditoModel() {
+    }
+
+    public Long getCodCereal() {
+        return codCereal;
+    }
+
+    public void setCodCereal(Long codCereal) {
+        this.codCereal = codCereal;
+    }
+
+    public Long getCodEmpresa() {
+        return codEmpresa;
+    }
+
+    public void setCodEmpresa(Long codEmpresa) {
+        this.codEmpresa = codEmpresa;
+    }
+
+    public String getCpfProdutor() {
+        return cpfProdutor;
+    }
+
+    public void setCpfProdutor(String cpfProdutor) {
+        this.cpfProdutor = cpfProdutor;
+    }
+
+    public Long getNumNotaFiscal() {
+        return numNotaFiscal;
+    }
+
+    public void setNumNotaFiscal(Long numNotaFiscal) {
+        this.numNotaFiscal = numNotaFiscal;
+    }
+
+    public Float getQtdComprada() {
+        return qtdComprada;
+    }
+
+    public void setQtdComprada(Float qtdComprada) {
+        this.qtdComprada = qtdComprada;
+    }
+
+    
+}
