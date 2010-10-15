66bce7a0f146dade37e6a78dd8f847af21356a1b
lk.kal@hotmail.com <lk.kal@hotmail.com@6a19b762-538a-c6d0-a5d2-96b9910417f3>
2010-10-15 22:53:15 +0000

    novas telas JSF

--- webservice/test/br/unoesc/ws/test/entitymanager/EntityManagerTest.java
+++ webservice/test/br/unoesc/ws/test/entitymanager/EntityManagerTest.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.unoesc.ws.test.entitymanager;
 
 import javax.persistence.EntityManager;
